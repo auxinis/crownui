910561cf7a3234e421a153ab683f0f64a51a0b9c
Nayeem <nayeem9812@gmail.com>
2021-10-05 23:09:14 +0600

    feat: initial library with example component

---
+++ site/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'crownui'
+import 'crownui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="UI Library 😎" />
+}
+
+export default App
